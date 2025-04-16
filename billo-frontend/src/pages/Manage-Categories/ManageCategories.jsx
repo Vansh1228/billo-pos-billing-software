5e8ffb2945a174582b618e441a1b7200c4c5395f
Vansh1228 <guptavansh1228@gmail.com>
2025-04-16 18:07:41 +0530

    added freontend changes

---
+++ billo-frontend/src/pages/Manage-Categories/ManageCategories.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ManageCategories = () => {
+  return (
+    <div className="category-container text-light">
+        <div className="left-coloumn">
+
+        </div>
+        <div className="right-coloumn">
+            
+        </div>
+    </div>
+  )
+}
+
+export default ManageCategories
